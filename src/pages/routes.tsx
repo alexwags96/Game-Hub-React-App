6fcfee3f25e1769777262a16da6092418ea7c8d0
Alexandre Wagane NDIAYE <wagane1996@gmail.com>
2023-06-20 20:37:40 +0200

    Handle errors

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 import GameDetailPage from "./GameDetailPage";
 import HomePage from "./HomePage";
 import Layout from "./Layout";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
